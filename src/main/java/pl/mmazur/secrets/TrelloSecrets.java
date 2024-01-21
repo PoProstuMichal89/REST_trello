5c5e58be178ff0052a614a212a1f79ef8e9b9b7c
Michał Mazur <michumazur89@gmail.com>
2024-01-21 12:13:44 +0100

    first commit

---
+++ src/main/java/pl/mmazur/secrets/TrelloSecrets.java
@@ -0,0 +1,19 @@
+package pl.mmazur.secrets;
+
+public class TrelloSecrets {
+
+    private TrelloSecrets() {
+
+    }
+
+    private static final String KEY = "616ae19a7c06bb54c643bda8716af5ce";
+    private static final String TOKEN = "ATTA92d66891168ad006998aa1a680fc77852ac92b3f483cdf091fba2fb9b8ff207eCDBF510B";
+
+    public static String getKEY() {
+        return KEY;
+    }
+
+    public static String getTOKEN() {
+        return TOKEN;
+    }
+}
---
+++ src/test/java/pl/mmazur/tests/board/CreateBoardTest.java
@@ -0,0 +1,53 @@
+package pl.mmazur.tests.board;
+
+import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import pl.mmazur.secrets.TrelloSecrets;
+import pl.mmazur.url.TrelloUrl;
+
+import static io.restassured.RestAssured.given;
+
+public class CreateBoardTest {
+
+    private final String boardName = "Testowy board1";
+    private String boardId;
+
+
+
+    @Test
+    void createBoardTest() {
+
+        final Response response = given()
+                .contentType(ContentType.JSON)
+                .queryParam("key", TrelloSecrets.getKEY())
+                .queryParam("token", TrelloSecrets.getTOKEN())
+                .queryParam("name", boardName)
+                .when()
+                .post(TrelloUrl.getBoardsUrl())
+                .then()
+                .extract()
+                .response();
+
+        Assertions.assertEquals(200, response.statusCode());
+
+        JsonPath json = response.jsonPath();
+        Assertions.assertEquals(boardName, json.getString("name"));
+        boardId =json.getString("id");
+
+        //Delete board
+        given()
+                .contentType(ContentType.JSON)
+                .queryParam("key", TrelloSecrets.getKEY())
+                .queryParam("token", TrelloSecrets.getTOKEN())
+                .when()
+                .delete(TrelloUrl.getBoardUrl(boardId))
+                .then()
+                .extract()
+                .response();
+
+        Assertions.assertEquals(200, response.statusCode());
+    }
+}
