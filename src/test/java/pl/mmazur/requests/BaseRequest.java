a43e49c5dbd0f12c1d7e5ed0269148650666386c
Michał Mazur <michumazur89@gmail.com>
2024-01-21 21:16:21 +0100

    Parametryzowanie testów z pomcą strumienia

--- src/test/java/pl/mmazur/requests/BaseRequest.java
+++ src/test/java/pl/mmazur/requests/BaseRequest.java
@@ -1,6 +1,8 @@
 package pl.mmazur.requests;
 
 import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.filter.log.RequestLoggingFilter;
+import io.restassured.filter.log.ResponseLoggingFilter;
 import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
 import pl.mmazur.secrets.TrelloSecrets;
@@ -13,6 +15,8 @@ public class BaseRequest {
         requestSpecBuilder.setContentType(ContentType.JSON);
         requestSpecBuilder.addQueryParam("key", TrelloSecrets.getKey());
         requestSpecBuilder.addQueryParam("token", TrelloSecrets.getToken());
+        requestSpecBuilder.addFilter(new RequestLoggingFilter());
+        requestSpecBuilder.addFilter(new ResponseLoggingFilter()); // --> dodawanie logowania requestów i response'ów
 
         return requestSpecBuilder.build();
     }
--- src/test/java/pl/mmazur/tests/board/CreateBoardTest.java
+++ src/test/java/pl/mmazur/tests/board/CreateBoardTest.java
@@ -4,12 +4,17 @@ package pl.mmazur.tests.board;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import pl.mmazur.requests.board.CreateBoardRequest;
 import pl.mmazur.requests.board.DeleteBoardRequest;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Stream;
 
 public class CreateBoardTest {
 
@@ -24,17 +29,52 @@ public class CreateBoardTest {
 
         final Response response = CreateBoardRequest.createBoardRequest(queryParams);
 
-//        Assertions.assertEquals(200, response.statusCode());
+//        Assertions.assertEquals(200, response.statusCode()); --> asercja z junita
         Assertions.assertThat(response.statusCode()).isEqualTo(200);
         JsonPath json = response.jsonPath();
-//        Assertions.assertEquals(boardName, json.getString("name"));
+//        Assertions.assertEquals(boardName, json.getString("name")); -> asercja z junita
         Assertions.assertThat((json.getString("name"))).isEqualTo(boardName);
         boardId = json.getString("id");
 
         //Delete board
         final Response deleteResponse = DeleteBoardRequest.deleteBoardRequest(boardId);
 
-//        Assertions.assertEquals(200, deleteResponse.statusCode());
+//        Assertions.assertEquals(200, deleteResponse.statusCode()); -> asercja z junita
         Assertions.assertThat(deleteResponse.statusCode()).isEqualTo(200);
     }
+
+    @DisplayName("Create board with valid data")
+    @ParameterizedTest(name = "Board name: {0}") //wyświetla nazwę testu zamiast nazwy metody
+    @MethodSource("sampleBoardNameData")
+    void createBoard1Test(String boardName) {
+        Map<String, String> queryParams = new HashMap<>();
+        queryParams.put("name", boardName);
+
+        final Response response = CreateBoardRequest.createBoardRequest(queryParams);
+        Assertions.assertThat(response.statusCode()).isEqualTo(200);
+        JsonPath json = response.jsonPath();
+        Assertions.assertThat((json.getString("name"))).isEqualTo(boardName);
+        boardId = json.getString("id");
+
+        //Delete board
+        final Response deleteResponse = DeleteBoardRequest.deleteBoardRequest(boardId);
+        Assertions.assertThat(deleteResponse.statusCode()).isEqualTo(200);
+    }
+
+    //metoda zwraca strumień danych,  używany potem w metodzie createBoard1Test do podstawiania danych testowych
+    private static Stream<Arguments> sampleBoardNameData() {
+        return Stream.of(
+                Arguments.of("Testowy Board"),
+                Arguments.of("!"),
+                Arguments.of("@"),
+                Arguments.of("#"),
+                Arguments.of("$"),
+                Arguments.of("%"),
+                Arguments.of("^"),
+                Arguments.of("Testowa1"),
+                Arguments.of("TESTOWA2")
+        );
+    }
+
+
 }
